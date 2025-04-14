d758a7d1a13f3e6fa81201c00719cbbd1582be61
CorySmith97 <corysmith7991@gmail.com>
2025-04-14 16:31:15 -0600

    Small changes

---
+++ src/scripting/entity_api.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+const Lua = @import("lua.zig");
+const Entity = @import("../entity.zig");
+const c = @import("c.zig");
+
+export fn setMetaTable(state: *Lua, e: *Entity) void {
+    c.lua_newtable(state);
+
+    // Setup the getId function with entity as upvalue
+    c.lua_pushlightuserdata(state, &e);
+    // format for adding methods
+    //c.lua_pushcclosure(state, getId, 1);
+    //c.lua_setfield(state, -2, "getId");
+
+    // Set the table as a global named "entity"
+    c.lua_setglobal(state, "entity");
+}
